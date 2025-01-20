4c3c71c08c0b949b340010df153f25ee4e70e83d
Florian Brandner <florian@brandner.net>
2025-01-20 11:05:59 +0100

    Clean-Up

--- QMK_RGB/keymaps/default/keymap.c
+++ QMK_RGB/keymaps/default/keymap.c
@@ -33,7 +33,6 @@
 
 enum layers {
     _STD = 0,
-    _QWERTY,
     _ALT,
     _SET,
     _CRS,
@@ -77,22 +76,12 @@ const uint32_t unicode_map[] PROGMEM = {
 #define U_OE UP(U_OE_LOWER, U_OE_UPPER)
 #define U_SS UP(U_SS_LOWER, U_SS_UPPER)
 
-/**************************************************** TAP DANCES */
-
-enum {
-    TD_GRV,
-};
-
-tap_dance_action_t tap_dance_actions[] = {
-  [TD_GRV]   = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),     // ESC, GRV on double tap
-};
-
 /**************************************************** THE LAYERS */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_STD] = LAYOUT(
   //,---------------------------------------------------.                     ,-----------------------------------------------------.
-    TD(TD_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
     LT(_SET,KC_TAB),KC_Q,KC_W,    KC_F,    KC_P,    KC_G,                          KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,MO(_CRS),
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
@@ -103,22 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT,   KC_SPC,     KC_ENT, KC_RSFT, KC_RALT,  KC_EQL, KC_MINS
   //                \--------+--------+--------+--------+---------|   |-------+--------+--------+--------+-------/
 ),
-  [_QWERTY] = LAYOUT(
-  //,---------------------------------------------------.                     ,-----------------------------------------------------.
-    TD(TD_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-  //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-    LT(_SET,KC_TAB),KC_Q,KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,MO(_CRS),
-  //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-    LT(_ALT,KC_PAUSE),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-     KC_DEL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,    _______,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-  //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-                      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT,   KC_SPC,     KC_ENT, KC_RSFT, KC_RALT,  KC_EQL, KC_MINS
-  //                \--------+--------+--------+--------+---------|   |-------+--------+--------+--------+-------/
-),
   [_ALT] = LAYOUT(
   //,---------------------------------------------------.                     ,-----------------------------------------------------.
-    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+    KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
     _______, _______, _______, _______, _______, _______,                       _______, _______,    U_UE, _______, _______,  KC_F12,
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
@@ -144,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
   [_CRS] = LAYOUT(
   //,---------------------------------------------------.                     ,-----------------------------------------------------.
-    _______, _______, _______,_______,_______,_______,                      TG(_QWERTY), _______, _______, _______, _______, _______,
+    _______, _______, _______,_______,_______,_______,                          _______, _______, _______, _______, _______, _______,
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
     _______, KC_HOME,   KC_UP,  KC_END, _______, KC_PGUP,                       _______, _______, _______, _______, _______, _______,
   //|------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
@@ -251,10 +227,6 @@ static void print_left(void) {
         oled_write_char(0xB9, false);
         oled_write_char(0xBA, false);
         oled_write_ln_P(PSTR("  "), false);
-    }
-    // QWERTY Functions Layer: Layer name
-    else if (IS_LAYER_ON(_QWERTY)) {
-      oled_write_ln_P(PSTR("QWERT"), true);
     }
     // ALT Functions Layer: Layer name
     else if (IS_LAYER_ON(_ALT)) {
@@ -416,20 +388,19 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 // Downlight: Blue
 // Keylights:
 //    Standard Layer: Configured Effect, blue downlights
-//    QWERTY Layer:   Configured Effect, blue downlights
 //    ALT Layer:      RED for special keys
-//    Cursor Layer:   GREEN for special keys
 //    Setting Layer:  PURPLE for special keys
+//    Cursor Layer:   GREEN for special keys
 //
 // Note: The RGB variable stores in different byte order, so the order in the
 // set_color call is different!
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   // Target color for key and downlight leds (one for every layer)
-  RGB ledcol[NUM_LAYERS] = { {RGB_BLUE}, {RGB_CYAN}, {RGB_RED}, {RGB_PURPLE}, {RGB_GREEN} } ;
+  RGB ledcol[NUM_LAYERS] = { {RGB_BLUE}, {RGB_RED}, {RGB_PURPLE}, {RGB_GREEN} } ;
   uint8_t layer = get_highest_layer(layer_state);
 
   // For special layers: Set the key LEDs, overwriting effects
-  if (layer > _QWERTY) {
+  if (layer > _STD) {
     for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
       for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
         uint8_t index = g_led_config.matrix_co[row][col];
